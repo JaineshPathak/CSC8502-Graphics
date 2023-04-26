2761148cf39e34823002c5975d1aabcd6ac1a946
JaineshPathak <jaineshpathak@gmail.com>
2023-04-26 14:54:29 +0100

    Add - Skeletal Blending

--- 9) Skeletal Animation/Renderer.cpp
+++ 9) Skeletal Animation/Renderer.cpp
@@ -3,8 +3,18 @@
 #include "../nclgl/MeshAnimation.h"
 #include "../nclgl/MeshMaterial.h"
 
-Renderer::Renderer(Window& parent) : OGLRenderer(parent)
+Renderer::Renderer(Window& parent) : w(parent), OGLRenderer(parent)
 {
+	//-----------------------------------------------------------
+	//Imgui 
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplWin32_Init(parent.GetHandle());
+	ImGui_ImplOpenGL3_Init("#version 330");
+	//-----------------------------------------------------------
+
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
@@ -16,9 +26,11 @@ Renderer::Renderer(Window& parent) : OGLRenderer(parent)
 	if (!shader->LoadSuccess())
 		return;
 
-	mesh = Mesh::LoadFromMeshFile("Role_T.msh");
-	anim = new MeshAnimation("Role_T.anm");
-	material = new MeshMaterial("Role_T.mat");
+	mesh = Mesh::LoadFromMeshFile("Character_Boss.msh");
+	material = new MeshMaterial("Character_Boss.mat");
+	anim = new MeshAnimation("Boss_Gun_Dance.anm");
+	anim2 = new MeshAnimation("Boss_Gun_Run.anm");
+	jumpAnim = new MeshAnimation("Boss_Gun_Jump.anm");
 
 	for (int i = 0; i < mesh->GetSubMeshCount(); i++)
 	{
@@ -36,7 +48,16 @@ Renderer::Renderer(Window& parent) : OGLRenderer(parent)
 
 	currentFrame = 0;
 	frameTime = 0.0f;
+	playAutomatic = false;
+
+	editMode = false;
+	blendFactor = 0.0f;
 	init = true;
+
+	currentAnim = anim;
+	previousAnim = nullptr;
+	pendingAnim = nullptr;
+	isTweening = false;
 }
 
 Renderer::~Renderer(void)
@@ -46,6 +67,13 @@ Renderer::~Renderer(void)
 	delete anim;
 	delete material;
 	delete shader;
+
+	//-----------------------------------------------------------
+	//Imgui 
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+	//-----------------------------------------------------------
 }
 
 void Renderer::RenderScene()
@@ -59,11 +87,59 @@ void Renderer::RenderScene()
 
 	vector<Matrix4> frameMatrices;
 
-	const Matrix4* invBindPose = mesh->GetInverseBindPose();
-	const Matrix4* frameData = anim->GetJointData(currentFrame);
+	if (editMode)
+	{
+		/*Matrix4 currentFrameMat = *(anim->GetJointData(currentFrame));
+		Matrix4 currentFrameMatInverse = mesh->GetInverseBindPose()[currentFrame];
+		finalBlend = LerpMat(currentFrameMat, lastFrame, blendFactor);
+		finalBlendInverse = LerpMat(currentFrameMatInverse, lastFrameInverse, blendFactor);*/
+
+		/*Matrix4 anim1lastFrameMat = *(anim->GetJointData(anim->GetFrameCount() - 1));
+		Matrix4 anim2FirstFrameMat = *(anim2->GetJointData(0));
+
+		Matrix4 anim1lastFrameMatInv = mesh->GetInverseBindPose()[anim->GetFrameCount() - 1];
+		Matrix4 anim2FirstFrameMatInv = mesh->GetInverseBindPose()[0];
+
+		finalBlend = LerpMat(anim1lastFrameMat, anim2FirstFrameMat, blendFactor);
+		finalBlendInverse = LerpMat(anim1lastFrameMatInv, anim2FirstFrameMatInv, blendFactor);*/
+
+		const Matrix4* anim1Frame = anim->GetJointData(currentFrame);
+		const Matrix4* anim2Frame = anim2->GetJointData(0);
+
+		const Matrix4* invBindPose = mesh->GetInverseBindPose();
+
+		std::vector<Matrix4> finalBlending;
+		for (size_t i = 0; i < mesh->GetJointCount(); i++)
+			finalBlending.emplace_back(LerpMat(anim1Frame[i], anim2Frame[i], blendFactor));
+
+		for (size_t i = 0; i < mesh->GetJointCount(); i++)
+			frameMatrices.emplace_back(finalBlending[i] * invBindPose[i]);
+	}
+	else
+	{
+		if (isTweening)
+		{
+			const Matrix4* anim1Frame = currentAnim->GetJointData(currentFrame);
+			const Matrix4* anim2Frame = pendingAnim->GetJointData(0);
+
+			const Matrix4* invBindPose = mesh->GetInverseBindPose();
+
+			std::vector<Matrix4> finalBlending;
+			for (size_t i = 0; i < mesh->GetJointCount(); i++)
+				finalBlending.emplace_back(LerpMat(anim1Frame[i], anim2Frame[i], blendFactor));
+
+			for (size_t i = 0; i < mesh->GetJointCount(); i++)
+				frameMatrices.emplace_back(finalBlending[i] * invBindPose[i]);
+		}
+		else
+		{
+			const Matrix4* invBindPose = mesh->GetInverseBindPose();
+			const Matrix4* frameData = currentAnim->GetJointData(currentFrame);
 	
-	for (unsigned int i = 0; i < mesh->GetJointCount(); i++)
-		frameMatrices.emplace_back(frameData[i] * invBindPose[i]);
+			for (unsigned int i = 0; i < mesh->GetJointCount(); i++)
+				frameMatrices.emplace_back(frameData[i] * invBindPose[i]);
+		}
+	}
 
 	int j = glGetUniformLocation(shader->GetProgram(), "joints");
 	glUniformMatrix4fv(j, frameMatrices.size(), false, (float*)frameMatrices.data());
@@ -74,19 +150,95 @@ void Renderer::RenderScene()
 		glBindTexture(GL_TEXTURE_2D, matTextures[i]);
 		mesh->DrawSubMesh(i);
 	}
+
+	RenderImGui();
+}
+
+void Renderer::RenderImGui()
+{
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	if (ImGui::CollapsingHeader("Animations"))
+	{		
+		bool editMod = editMode;				
+		if (ImGui::Checkbox("Edit Mode", (bool*)&editMod)) editMode = editMod;
+		if (editMode)
+		{
+			float blend = blendFactor;
+			if (ImGui::DragFloat("Blend Factor", (float*)&blend, 0.05f, 0.0f, 1.0f)) blendFactor = blend;
+		}
+		else
+		{
+			bool playAuto = playAutomatic;
+			int curFrame = currentFrame;
+			if (ImGui::Checkbox("Play Automatic", (bool*)&playAuto)) playAutomatic = playAuto;
+			if (ImGui::DragInt("Current Frame", (int*)&curFrame, 1.0f, 0, anim->GetFrameCount() - 1)) currentFrame = curFrame;
+		}
+	}
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void Renderer::UpdateScene(float dt)
 {
+	globalDt = dt;
 	elaspedTime += dt;
 
 	camera->UpdateCamera(dt);
 	viewMatrix = camera->BuildViewMatrix();
 
-	frameTime -= dt;
-	while (frameTime < 0.0f)
+	if (playAutomatic && !editMode && !isTweening)
 	{
-		currentFrame = (currentFrame + 1) % anim->GetFrameCount();
-		frameTime += 1.0f / anim->GetFrameRate();
+		frameTime -= dt;
+		while (frameTime < 0.0f)
+		{
+			currentFrame = (currentFrame + 1) % currentAnim->GetFrameCount();
+			frameTime += 1.0f / currentAnim->GetFrameRate();
+		}
 	}
+
+	if (Window::GetKeyboard()->KeyTriggered(KeyboardKeys::KEYBOARD_1)) PlayAnim(anim);
+	if (Window::GetKeyboard()->KeyTriggered(KeyboardKeys::KEYBOARD_2)) PlayAnim(anim2);
+	if (Window::GetKeyboard()->KeyTriggered(KeyboardKeys::KEYBOARD_3)) PlayAnim(jumpAnim);
+
+	if (isTweening && pendingAnim != nullptr)
+	{
+		blendFactor += dt * (1 / tweenTime);		
+		std::cout << "Blend Factor: " << blendFactor << std::endl;
+		tweenTimeCurrent -= dt;
+		if (tweenTimeCurrent <= 0.0f)
+		{
+			tweenTimeCurrent = 0.0f;
+			isTweening = false;
+			currentFrame = 0;
+			currentAnim = pendingAnim;
+			std::cout << "Tweening COMPLETE!\n";
+		}
+	}
+
+	if (ImGui::GetIO().MouseClicked[1])
+	{
+		showCursor = !showCursor;
+		ImGui::GetIO().MouseDrawCursor = showCursor;
+		w.LockMouseToWindow(!showCursor);
+	}
+}
+
+void Renderer::PlayAnim(MeshAnimation* newAnim)
+{
+	previousAnim = currentAnim;
+	pendingAnim = newAnim;
+
+	TweenAnim(previousAnim, pendingAnim, 0.15f);
+}
+
+void Renderer::TweenAnim(MeshAnimation* _previousAnim, MeshAnimation* _nextAnim, float time)
+{
+	isTweening = true;
+	blendFactor = 0.0f;
+	tweenTimeCurrent = time;
+	tweenTime = time;
 }
--- 9) Skeletal Animation/Renderer.h
+++ 9) Skeletal Animation/Renderer.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "..\nclgl\OGLRenderer.h"
+#include "../Third Party/imgui/imgui.h"
+#include "../Third Party/imgui/imgui_impl_opengl3.h"
+#include "../Third Party/imgui/imgui_impl_win32.h"
+
+#include <thread>
+#include <atomic>
 
 class Camera;
 class Mesh;
@@ -13,20 +19,56 @@ public:
 	~Renderer(void);
 
 	void RenderScene() override;
+	void RenderImGui();
 	void UpdateScene(float dt) override;
 
 protected:
+	Matrix4 LerpMat(const Matrix4& a, const Matrix4& b, float t)
+	{
+		Matrix4 res;
+
+		for (int i = 0; i < 16; i++)
+		{
+			res.values[i] = naive_lerp(a.values[i], b.values[i], t);
+		}
+
+		return res;
+	}
+
 	Camera* camera;
 	Mesh* mesh;
 	Shader* shader;
 	MeshAnimation* anim;
+	MeshAnimation* anim2;
+	MeshAnimation* jumpAnim;
+	MeshAnimation* currentAnim;
+	MeshAnimation* previousAnim;
+	MeshAnimation* pendingAnim;
 	MeshMaterial* material;
 	std::vector<GLuint> matTextures;
 
+	Window& w;
+	bool showCursor;
+	bool playAutomatic;
+	bool editMode;
+
+	float blendFactor;
 	int currentFrame;
 	float frameTime;
 	float elaspedTime;
 
+	Matrix4 finalBlend;
+	Matrix4 finalBlendInverse;
+
+	std::atomic<float> globalDt;
+	
+	bool isTweening;
+	std::thread tweenThread;
+	float tweenTimeCurrent;
+	float tweenTime;
+
+	void PlayAnim(MeshAnimation* newAnim);
+	void TweenAnim(MeshAnimation* previousAnim, MeshAnimation* nextAnim, float time);
 	std::vector<Vector3> framePosList;
 
 	//const Matrix4* GetInterpolatedAnim(Vector3 a, Vector3 b);
